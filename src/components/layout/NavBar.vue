0fb8a77548b5e8601bfadb646d4dd4ac020d55c7
gabriel_chagas <112498725+GabrielChagasNext@users.noreply.github.com>
2022-12-01 20:53:27 -0300

    added fontawesome

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,32 +1,65 @@
+<script setup>
+/* 
+  imports
+*/
+import { ref } from "vue";
+
+/* 
+  navbar mobile
+*/
+
+const showMobileNav = ref(false);
+</script>
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item" href="https://bulma.io">
-        Notes
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">
+          <font-awesome-icon icon="note-sticky" />
+          Notes
+        </div>
+
+        <a
+          @click.prevent="(showMobileNav = !showMobileNav)"
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileNav }"
       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-end">
-        <RouterLink to="/" class="navbar-item" active-class="is-active">
-          Notes
-        </RouterLink>
-        <RouterLink to="/stats" class="navbar-item" active-class="is-active">
-          Stats
-        </RouterLink>
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item" active-class="is-active">
+            Notes
+          </RouterLink>
+          <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+            Stats
+          </RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+<style>
+.navbar{
+  user-select: none;
+}
+@media (max-width:1023px) {
+  .navbar-menu{
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
